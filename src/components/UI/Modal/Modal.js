d32f038770eef38432d02f393ae9228ccf5bc67c
baodang <baodq96@gmail.com>
2020-08-31 17:43:58 +0700

    Add shouldComponentUpdate to enhance Modal component render multi time

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Modal.module.css";
 import Backdrop from "../Backdrop/Backdrop";
 import Aux from "../../../hoc/Aux";
 
-const modal = (props) => {
-  return (
-    <Aux>
-      <Backdrop clicked={props.closeModal} show={props.show} />
-      <div
-        style={{
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: props.show ? "1" : "0",
-        }}
-        className={classes.Modal}
-      >
-        {props.children}
-      </div>
-    </Aux>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
-export default modal;
+  render() {
+    return (
+      <Aux>
+        <Backdrop clicked={this.props.closeModal} show={this.props.show} />
+        <div
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0",
+          }}
+          className={classes.Modal}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
+
+export default Modal;
